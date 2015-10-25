0afd32ec3c9a7f95e53fd8cd304a060aa1715349
Shane Martin <ghoulminy@live.com>
2015-10-25 16:57:12 +1000

    Ghost Grabbing

--- Assets/Scripts/GhostControl.cs
+++ Assets/Scripts/GhostControl.cs
@@ -3,9 +3,29 @@ using System.Collections;
 
 public class GhostControl : MonoBehaviour
 {
+	private PlayerController player;
+	public PlayerController heldPlayer;
+	bool grabbed = false;
 	bool inWall = false;
 	float timer;
-	float defaultTimer = .75f;
+	float defaultTimer = .25f;
+	float holdTimer = 3;
+	float holdTimerDefault = 3;
+	
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		PlayerController touchedPlayer = col.gameObject.GetComponent<PlayerController> ();
+		if (col.gameObject.GetComponent<WallController> () != null) {
+			TouchedWall ();
+		} else if (touchedPlayer != null) {
+			touchedPlayer.canMove = false;
+			heldPlayer = touchedPlayer;
+			GrabbedPlayer ();
+			grabbed = true;
+			holdTimer = holdTimerDefault;
+		}
+	}
+	
 
 
 	public void TouchedWall ()
@@ -15,9 +35,28 @@ public class GhostControl : MonoBehaviour
 		timer = defaultTimer;
 	}
 	
+	void GrabbedPlayer ()
+	{
+		heldPlayer.transform.parent = this.transform;
+		heldPlayer.GetComponent<Collider2D> ().enabled = false;
+	}
+	
+	void LetGoOfPlayer ()
+	{
+		grabbed = false;
+		heldPlayer.transform.parent = null;
+		heldPlayer.GetComponent<Collider2D> ().enabled = true;
+		heldPlayer = null;
+	}
 	public void LeftWall ()
 	{
 		GetComponent<Collider2D> ().enabled = true;
+	}
+	
+	
+	void Awake ()
+	{
+		player = this.GetComponent<PlayerController> ();
 	}
 	// Use this for initialization
 	void Start ()
@@ -36,6 +75,13 @@ public class GhostControl : MonoBehaviour
 				LeftWall ();
 			}
 		}
-	
+		
+		if (grabbed) {
+			holdTimer -= Time.deltaTime;
+			if (holdTimer < 0) {
+				LetGoOfPlayer ();
+			}
+		}
+		
 	}
 }
