bf471a3df2bb38c183ee1789eefa38c0d500baa1
Shane Martin <ghoulminy@live.com>
2015-10-25 16:07:39 +1000

    Wall Movement! Ghost can move through walls

---
+++ Assets/Scripts/GhostControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostControl : MonoBehaviour
+{
+	bool inWall = false;
+	float timer;
+	float defaultTimer = .75f;
+
+
+	public void TouchedWall ()
+	{
+		GetComponent<Collider2D> ().enabled = false;
+		inWall = true;
+		timer = defaultTimer;
+	}
+	
+	public void LeftWall ()
+	{
+		GetComponent<Collider2D> ().enabled = true;
+	}
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (inWall) {
+			timer -= Time.deltaTime;
+			if (timer < 0) {
+				inWall = false;
+				LeftWall ();
+			}
+		}
+	
+	}
+}
