916e0d29ae7750ec8940f46bccad146554a52b00
Shane Martin <ghoulminy@live.com>
2015-10-25 19:35:47 +1000

    Adding in stun from flashlight

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : SingletonBehaviour<GameController>
+{
+	public static event System.Action<float> OnTimeChange;
+	public List<GhostControl> GhostList = new List<GhostControl> ();
+	
+	protected override void OnSingletonAwake ()
+	{
+		
+	}
+}
--- Assets/Scripts/PushControl.cs
+++ Assets/Scripts/PushControl.cs
@@ -7,9 +7,14 @@ public class PushControl : MonoBehaviour
 	
 	private WallController heldWall;
 	public WallController targetedWall;
-	public LayerMask collidersHit;
 	public float grabDist = 1;
 	public float moveModifier = .7f;
+	
+	public float visionDistance = 10;
+	public float visionCone = 30;
+	public LayerMask collidersHit;
+
+	
 	
 	void Awake ()
 	{
@@ -41,7 +46,7 @@ public class PushControl : MonoBehaviour
 	
 	bool CheckWall ()
 	{
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.forward, grabDist, collidersHit);
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.forward, grabDist, collidersHit.value);
 		Debug.DrawRay (transform.position, transform.forward, Color.blue);
 		if (hit != null) {
 			if (hit.collider != null) {
@@ -64,6 +69,38 @@ public class PushControl : MonoBehaviour
 		} else if (targetedWall != null && CheckWall ()) {
 			heldWall = targetedWall;
 			player.canMove = false;
+		}
+	}
+	void SearchForEnemyGhost ()
+	{
+		for (int i = 0; i < GameController.Instance.GhostList.Count; ++i) {
+			GameController.Instance.GhostList [i];
+			
+			Vector3 deltaToPlayer = (list[i].transform.position - transform.position);
+			Vector3 directionToPlayer = deltaToPlayer.normalized;
+			
+			float dot = Vector3.Dot (transform.up, directionToPlayer);
+			float cone = Mathf.Cos(visionCone/2 * Mathf.Deg2Rad);
+			
+			if(dot > cone){
+				float distance = Vector3.Distance (transform.position, list[i].transform.position);
+				if (distance < visionDistance) {
+					Physics2D.raycastsHitTriggers = false;
+					RaycastHit2D hit = Physics2D.Raycast(transform.position + transform.up, directionToPlayer, visionDistance, collidersHit.value);
+					if (hit != null) {
+						if(list[i] != null){
+							if(hit.collider.gameObject == list[i].gameObject){
+								targetLastPosition = list[i];
+								AITargetting ai = list[i].GetComponent<AITargetting>();
+								if(ai != null){
+									ai.CurrentlyTargetted(this.gameObject);
+								}
+								timeWithoutSeeingTarget = timeWithoutSeeingTargetValue;
+							}
+						}
+					}
+				}
+				
 		}
 	}
 }
