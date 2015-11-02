2124da8940f6fc4ca202cd2d3818adda44982ebf
Shane Martin <ghoulminy@live.com>
2015-11-02 18:14:29 +1000

    Fixed some things

--- Assets/Scripts/Spells/DamageSpell.cs
+++ Assets/Scripts/Spells/DamageSpell.cs
@@ -4,25 +4,23 @@ using System.Collections;
 public class DamageSpell : Spell
 {
 	public BallSpell damageBall;
+	public AoEBallSpell damageBallAoE;
 	public Transform spawnPoint;
 	public float damage = 20f;
 	
 	
 	public override void HoldCast ()
 	{
-		if(!onCooldown){
-		if (!canCast) {
-			castTime -= Time.deltaTime;
-			if (castTime <= 0) {
-				canCast = true;
+		if (!onCooldown) {
+			if (!canCast) {
+				castTime -= Time.deltaTime;
+				if (castTime <= 0) {
+					canCast = true;
+				}
 			}
 		}
-		}
 		if (canCast) {
-			BallSpell ball = Instantiate (damageBall, spawnPoint.transform.position, transform.rotation) as BallSpell;
-			ball.SetDamage (damage);
-			CooldownValues();
-			ResetValues ();
+			Shoot ();
 		}
 	}
 	public override void ReleaseCast ()
@@ -30,11 +28,26 @@ public class DamageSpell : Spell
 		if (canCast) {
 			BallSpell ball = Instantiate (damageBall, spawnPoint.transform.position, transform.rotation) as BallSpell;
 			ball.SetDamage (damage);
-			CooldownValues();
+			CooldownValues ();
 		}
 		ResetValues ();
 	}
-
+	
+	void Shoot ()
+	{
+		if (damageBall != null) {
+			BallSpell ball = Instantiate (damageBall, spawnPoint.transform.position, transform.rotation) as BallSpell;
+			ball.SetDamage (damage);
+			CooldownValues ();
+			ResetValues ();
+		} else if (damageBallAoE != null) {
+			AoEBallSpell ball = Instantiate (damageBallAoE, spawnPoint.transform.position, transform.rotation) as AoEBallSpell;
+			ball.SetDamage (damage);
+			CooldownValues ();
+			ResetValues ();
+		}
+		
+	}
 
 	public override void ResetValues ()
 	{
