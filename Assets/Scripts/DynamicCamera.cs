88af222dac1664e59979bd4686bec3b0602422e0
Shane Martin <ghoulminy@live.com>
2015-11-07 01:40:36 +1000

    Dynamic camera now works with 3D

--- Assets/Scripts/DynamicCamera.cs
+++ Assets/Scripts/DynamicCamera.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 public class DynamicCamera : MonoBehaviour
 {
 	public List<GameObject> gameObjectsToFollow = new List<GameObject> ();
+	public float minDistance = 12;
+	private float modifier = 1.25f;
+	
 	void Awake ()
 	{
 		DontDestroyOnLoad (this.gameObject);
@@ -15,36 +18,40 @@ public class DynamicCamera : MonoBehaviour
 	{
 		if (gameObjectsToFollow.Count > 0) {
 			FollowGameObjects ();
-			AdjustCameraSize ();
 		}
 	}
 	
 	void FollowGameObjects ()
 	{
-		Vector3 positionAverage = Vector3.zero;
-		for (int i = 0; i < gameObjectsToFollow.Count; ++i) {
-			positionAverage += gameObjectsToFollow [i].transform.position;
-		}
-		positionAverage /= gameObjectsToFollow.Count;
-		positionAverage.z = transform.position.z;
-		transform.position = positionAverage;
+		Vector3 pos1ForAverage = Vector3.zero;
+		Vector3 pos2ForAverage = Vector3.zero;
 		
-	}
-	void AdjustCameraSize ()
-	{
 		float currentMaxDistance = 0;
 		for (int i= 0; i < gameObjectsToFollow.Count; ++i) {
 			for (int j = 0; j < gameObjectsToFollow.Count; ++j) {
 				if (j != i) {
 					float distance = Vector3.Distance (gameObjectsToFollow [i].transform.position, gameObjectsToFollow [j].transform.position);
-					if (distance > currentMaxDistance) {
+					if (distance >= currentMaxDistance) {
 						currentMaxDistance = distance;
+						pos1ForAverage = gameObjectsToFollow [i].transform.position;
+						pos2ForAverage = gameObjectsToFollow [j].transform.position;
 					}
 				}
 			}
 		}
-		Vector3 pos = transform.position;
-		pos.z = -currentMaxDistance * 1.35f;
+		Vector3 positionAverage = (pos1ForAverage + pos2ForAverage) / 2;
+		transform.position = positionAverage;
+		AdjustCameraSize (currentMaxDistance);
+		
+	}
+	void AdjustCameraSize (float dist)
+	{
+		dist *= modifier;
+		if (dist < minDistance) {
+			dist = minDistance;
+		} 
+		Vector3 pos = transform.position + (-transform.forward * (dist));
+		
 		transform.position = pos;
 	}
 }
